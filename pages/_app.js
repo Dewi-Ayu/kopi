d03d9250dbc24ec46cfc489cf019d13604038766
Dewi-Ayu <74051114+Dewi-Ayu@users.noreply.github.com>
2022-01-19 17:43:52 +0700

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,12 @@
-// import '../styles/globals.css'
-
-// function MyApp({ Component, pageProps }) {
-//   return <Component {...pageProps} />
-// }
-
-// export default MyApp
-
-import Head from 'next/head';
+import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {useEffect} from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css'
-          rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossOrigin='anonymous'
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap")
+  })
+  return <Component {...pageProps} />
 }
-export default MyApp;
+
+export default MyApp
